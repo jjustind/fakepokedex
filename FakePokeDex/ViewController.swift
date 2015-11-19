de56f486082244635641661427b6252f6e824cbc
jjustind <jjustind@gmail.com>
2015-11-18 21:45:13 -0600

    Initial Commit

---
+++ FakePokeDex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FakePokeDex
+//
+//  Created by Justin on 11/18/15.
+//  Copyright © 2015 Justin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
